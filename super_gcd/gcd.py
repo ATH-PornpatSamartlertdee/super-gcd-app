b837dc58ed12d0c23c40050898cae83783bf7fba
Pornpat Samartlertdee <pornpat.samartlertdee@adastragrp.com>
2022-12-16 16:30:15 +0700

    feature: change formula and add unit test

--- super_gcd/gcd.py
+++ super_gcd/gcd.py
@@ -1,9 +1,13 @@
 def gcd(a, b):
     if type(a) != int or type(b) != int:
         return "You're shocked!"
-    while b > 0:
-        r=a%b
-        a=b
-        b=r
-        #(3,13), (13,3), (3,1)
-    return a
+    else:
+        if a < 0:
+            a = abs(a)
+        if b < 0:
+            b = abs(b)
+        while b > 0:
+            r=a%b
+            a=b
+            b=r
+        return a
--- tests/test_gcd.py
+++ tests/test_gcd.py
@@ -3,3 +3,68 @@ from super_gcd.gcd import gcd
 def test_gcd_of_3_and_6():
     result = gcd(3,6)
     assert result == 3
+
+def test_gcd_of_6_and_3():
+    result = gcd(6,3)
+    assert result == 3
+
+def test_gcd_of_4137_and_231252():
+    result = gcd(4137,231252)
+    assert result == 21
+
+def test_gcd_of_4_and_6():
+    result = gcd(4,6)
+    assert result == 2
+
+def test_gcd_of_6_and_4():
+    result = gcd(6,4)
+    assert result == 2
+
+def test_gcd_of_str3_and_6():
+    result = gcd('3',6)
+    assert result == "You're shocked!"
+
+def test_gcd_of_3_and_str6():
+    result = gcd(3,'6')
+    assert result == "You're shocked!"
+
+def test_gcd_of_0_and_5():
+    result = gcd(0,5)
+    assert result == 5
+
+def test_gcd_of_5_and_0():
+    result = gcd(5,0)
+    assert result == 5
+
+def test_gcd_of_0_and_neg5():
+    result = gcd(0,-5)
+    assert result == 5
+
+def test_gcd_of_neg5_and_0():
+    result = gcd(-5,0)
+    assert result == 5
+
+def test_gcd_of_neg7_and_neg21():
+    result = gcd(-7,-21)
+    assert result == 7
+
+def test_gcd_of_none_and_neg5():
+    result = gcd(None,-5)
+    assert result == "You're shocked!"
+
+def test_gcd_of_3_and_neg3():
+    result = gcd(3,-3)
+    assert result == 3
+
+def test_gcd_of_neg3_and_3():
+    result = gcd(-3,3)
+    assert result == 3
+
+def test_gcd_of_neg3_and_neg3():
+    result = gcd(-3,-3)
+    assert result == 3
+
+def test_gcd_of_neg3_and_neg3p5():
+    result = gcd(-3,-3.5)
+    assert result == "You're shocked!"
+
